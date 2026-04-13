c235f382e8525b05e1ed4c013c530c6ffa348cbc
Ghayath Alali <ghayathAlali2003@gmail.com>
2026-04-13 11:03:04 +0300

    Adding Entities repository

--- CarRental.Infrastructure/Repositories/Common/UnitOfWork.cs
+++ CarRental.Infrastructure/Repositories/Common/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using CarRental.Application.Repositories.Common;
+﻿using CarRental.Application.Repositories;
+using CarRental.Application.Repositories.Common;
 using CarRental.Infrastructure.Data;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,20 @@ namespace CarRental.Infrastructure.Repositories.Common
         {
             _Context = context;
         }
+
+        public ICustomerRepository CustomerRepository { get; }
+        public IFuleTypeRepository FuleTypeRepository { get; }
+
+        public IMaintenanceRepository MaintenanceRepository { get; }
+
+        public IRentalBookingRepository RentalBookingRepository { get; }
+        public IRentalTransactionRepository RentalTransactionRepository { get; }
+
+        public IVehicleCategoryRepository VehicleCategoryRepository { get; }
+
+        public IVehicleRepository VehicleRepository { get; }
+        public IVehicleReturnRepository VehicleReturnRepository { get; }
+
         public void Save()
         {
             _Context.SaveChanges();
---
+++ CarRental.Infrastructure/Repositories/FuleTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using CarRental.Application.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental.Infrastructure.Repositories
+{
+    public class FuleTypeRepository
+    {
+    }
+}
